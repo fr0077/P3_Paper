dc861724c14639c76b428e4d5908e278eefa0382
fr007 <fr00777@hotmail.co.jp>
2016-12-10 01:09:29 +0900

    echo off + エラー修正

--- make.bat
+++ make.bat
@@ -1,7 +1,8 @@
+echo off
 rem このバッチファイルを実行することでコンパイルします。
 rem 個々のTeXファイルはmain.texから呼び出します。
 rem したがって、個々のTeXファイルを単独でコンパイルすることはできません。
 
 ptex2pdf main.tex -u -l -ot -synctex=1 -no-guess-input-enc -kanji=utf8
 upbibtex main
-ptex2pdf main.tex -u -l -ot
+ptex2pdf main.tex -u -l
