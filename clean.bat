8339cbce2e72a8a07dbb5c5e2f99272bf3145b07
fr0077 <fr00777@hotmail.co.jp>
2016-12-10 01:21:07 +0900

    Add batch file (#1)
     コンパイル、クリーンアップ用のバッチファイルを追加

---
+++ clean.bat
@@ -0,0 +1,13 @@
+echo off
+rem TeXによって自動生成されるファイル（PDFを除く）を消去します。
+rem どれを編集すべきかわからなくなった時用。
+rem ここで消去されるファイルは、.gitignoreで無視するように設定しているため、
+rem プッシュ前にこれを実行する必要はありません。
+
+del *.aux
+del *.log
+del *.synctex.gz
+del *.out
+del *.blg
+del *.bbl
+del *.toc
