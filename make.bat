f0f03bdb5b067a49204dffadf7afc9d87693d412
maimya <mammy99-kutp@outlook.jp>
2017-03-22 00:38:24 +0900

    加速器加筆

--- make.bat
+++ make.bat
@@ -1,12 +1,7 @@
-@echo off
+﻿@echo off
 rem このバッチファイルを実行することでコンパイルします。
 rem 個々のTeXファイルはmain.texから呼び出します。
 rem したがって、個々のTeXファイルを単独でコンパイルすることはできません。
-
-<<<<<<< HEAD
-ptex2pdf -u -l -ot -synctex=1 -kanji=utf8 -no-guess-input-enc main
-rem upbibtex main
-=======
 ptex2pdf -u -l main.tex
 upbibtex main
->>>>>>> origin/master
+
